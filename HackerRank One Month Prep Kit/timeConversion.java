40ac02d688038f06cb325de4080cdda658388877
masis <121774583+007mva@users.noreply.github.com>
2023-01-02 16:53:48 -0500

    Update timeConversion.java

--- HackerRank One Month Prep Kit/timeConversion.java
+++ HackerRank One Month Prep Kit/timeConversion.java
@@ -1,4 +1,10 @@
-    public static String timeConversion(String s) {
+/* Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.    */
+
+
+public static String timeConversion(String s) {
     // Write your code here
     int hour = Integer.parseInt(s.substring(0, 2));
         String timeOfDay = s.substring(8, 10);
