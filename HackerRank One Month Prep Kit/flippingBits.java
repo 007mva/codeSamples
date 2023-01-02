4cb70fda20044293955d6ed1c62ed88094c338ea
masis <121774583+007mva@users.noreply.github.com>
2023-01-02 17:45:24 -0500

    Update flippingBits.java

--- HackerRank One Month Prep Kit/flippingBits.java
+++ HackerRank One Month Prep Kit/flippingBits.java
@@ -1,3 +1,8 @@
+/*
+You will be given a list of 32 bit unsigned integers. Flip all the bits (1->0 and 0->1 ) and return the result as an unsigned integer. 
+
+*/
+
 public static long flippingBits(long n) {
     // Write your code here
         
