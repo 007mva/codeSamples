9361512804f3adebd75889ebc819134c1ea92e68
masis <121774583+007mva@users.noreply.github.com>
2023-01-02 16:51:40 -0500

    Update minMaxSum.java

--- HackerRank One Month Prep Kit/minMaxSum.java
+++ HackerRank One Month Prep Kit/minMaxSum.java
@@ -1,4 +1,7 @@
-  public static void miniMaxSum(List<Integer> arr) {
+/* Given five positive integers, find the minimum and maximum 
+values that can be calculated by summing exactly four of the five integers */
+
+public static void miniMaxSum(List<Integer> arr) {
     // Write your code here
     long min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
     long sum=0;
