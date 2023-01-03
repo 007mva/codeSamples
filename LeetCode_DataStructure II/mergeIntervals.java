ea820b18f5f54ef7b6bf7a893a46f93086b2df0e
masis <121774583+007mva@users.noreply.github.com>
2023-01-03 18:50:56 -0500

    Add files via upload

---
+++ LeetCode_DataStructure II/mergeIntervals.java
@@ -0,0 +1,22 @@
+/*
+Given an array of intervals where intervals[i] = [starti, endi], 
+merge all overlapping intervals, and return an array of 
+the non-overlapping intervals that cover all the intervals in the input.
+*/
+
+public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+        LinkedList<int[]> l=new LinkedList();
+        
+        for(int interval[] :intervals){
+            if(l.isEmpty() || l.getLast()[1] < interval[0]){
+                l.add(interval);
+            }
+            else {
+                l.getLast()[1]=Math.max(l.getLast()[1], interval[1]);
+            }
+        }
+
+        return l.toArray(new int[l.size()][]);
+
+    }
