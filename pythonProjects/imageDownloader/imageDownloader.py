153a571b2a63ddea4841124b49cb52deeb933a1d
masis <121774583+007mva@users.noreply.github.com>
2023-01-04 16:30:46 -0500

    Add files via upload

---
+++ pythonProjects/imageDownloader/imageDownloader.py
@@ -0,0 +1,42 @@
+import bs4       #download beautiful soup
+import requests
+import os
+
+
+
+def image_downloader(extension):
+    """Search for and download all images of the argument type from Imgur."""
+    url = 'http://imgur.com/search/score/all?q=' + search
+    os.makedirs('/home/finners/Downloads/imgur', exist_ok=True)
+
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    image_elem = soup.select('.post > .image-list-link img')
+
+    for i, image in enumerate(image_elem):
+        # Convert image URL from thumbnail size to fullsize version
+        image_url_s = 'https:' + image_elem[i].get('src')
+        image_url = image_url_s[: -5] + '.' + extension
+
+        print('Downloading image {}'.format(image_url))
+        res = requests.get(image_url)
+        res.raise_for_status()
+        image_file = open(os.path.join('/home/finners/Downloads/imgur',
+                                        os.path.basename(image_url)), 'wb')
+        for chunk in res.iter_content(1000000):
+            image_file.write(chunk)
+        image_file.close()
+
+    return len(image_elem)
+
+
+search = input('Enter desired search keyword(s): ')
+downloaded = image_downloader('jpg') + image_downloader('png')
+
+if downloaded == 0:
+    print('no matching keyword.')
+
+else:
+    print('All ' + str(downloaded) + ' pictures successfully downloaded.')
